b2877c1857ccd809135cb0b77cd8b4b6da20e96a
simkass <simkassab@gmail.com>
2021-04-23 20:55:03 -0400

    Added post user api

--- covid_vaxx_qc_api.py
+++ covid_vaxx_qc_api.py
@@ -13,9 +13,21 @@ def hello_world():
     return jsonify({'message': 'Api Works!'})
 
 
-@app.route('/establishments/')
+@app.route('/establishments/', methods=['GET'])
 @cross_origin(headers=["Content-Type", "Authorization"])
 def get_establishments():
     postal_code = request.args.get('postal_code').replace(" ", "")
     location = clic_sante_api.get_geo_code(postal_code)['results'][0]['geometry']['location']
     return clic_sante_api.get_establishments(postal_code, location['lat'], location['lng'])
+
+@app.route('/user', methods=['POST'])
+@cross_origin(headers=["Content-Type"])
+def post_user():
+    response = request.get_json()
+    email_address = response['email']
+    establishments_of_interest = response['establishments']
+    availabilities = response['availabilities']
+    print(email_address)
+    print(establishments_of_interest)
+    print(availabilities)
+    return ''
