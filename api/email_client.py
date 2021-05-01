af7ff3f423dd19b53d5c07f41a3acf9d547f1b0e
simkass <simkassab@gmail.com>
2021-05-01 14:07:05 -0400

    Cleaned up email formats

--- api/email_client.py
+++ api/email_client.py
@@ -19,30 +19,35 @@ def email_login():
 def send_sign_up_email(sendto, establishments_of_interest, establishments, availabilities):
     smtpserver = email_login()
     message = MIMEMultipart("alternative")
-    message["Subject"] = "Vous venez de vous abonner à Alerte Vaccin QC"
+    message["Subject"] = "Abonnement à Alerte Vaccin QC"
     message["From"] = config.email_address
     message["To"] = sendto
 
     msg = """<html><body>
-                <p>Vous venez de vous abonner au service Alerte Vaccin QC</p>
-                <p>Vous recevrez un courriel lorsqu'apparaîtra un rendez-vous de vaccination qui respecte les critères suivant:</p>
-                <p><b>Cliniques de vaccination:</b></p>"""
+                <h2>Vous venez de vous abonner au service Alerte Vaccin QC</h2>
+                <h3>Vous recevrez un courriel lorsqu'apparaîtra un rendez-vous de vaccination qui respecte les critères suivants:</h3>
+                <h2>Cliniques de vaccination:</h2>
+                <table>
+                    <tr
+                        <th>Clinique</th>
+                        <th>Adresse</th>
+                    </tr>"""
 
     for place in establishments['places']:
         if place['id'] in establishments_of_interest:
-            msg = msg + """<p> - """ + place['name_fr'] + ": " + \
-                """<i>   """ + place['formatted_address'] + """</i></p>"""
+            msg = msg + """<tr><td>""" + place['name_fr'] + """</td><td>""" + \
+                place['formatted_address'] + """</td></tr>"""
 
-    msg = msg + """<p><b>Disponibilités: </b></p>"""
+    msg = msg + """</table><h2>Disponibilités: </h2>"""
 
     for availability in availabilities:
 
-        start_date, start_time = utils.get_datetime_strings(availability['start'])
-        end_date, end_time = utils.get_datetime_strings(availability['stop'])
+        start_date, start_time = utils.get_datetime_full_strings(availability['start'])
+        end_date, end_time = utils.get_datetime_full_strings(availability['stop'])
 
-        msg = msg + """<p>Du """ + start_date + " à " + start_time + " jusqu'au " + end_date + " à " + end_time + """</p>"""
+        msg = msg + """<h4>Du """ + start_date + " à " + start_time + " jusqu'au " + end_date + " à " + end_time + """</h4>"""
 
-    msg = msg + """</body></html>"""
+    msg = msg + """<p><b>Alerte Vaccin QC</b></p></body></html>"""
 
     html = MIMEText(msg, "html")
     message.attach(html)
@@ -54,22 +59,30 @@ def send_sign_up_email(sendto, establishments_of_interest, establishments, avail
 def send_notification_email(user, availabilities, establishments):
     smtpserver = email_login()
     message = MIMEMultipart("alternative")
-    message["Subject"] = "Ces rendez-vous de vaccination pourraient vous intéresser"
+    message["Subject"] = "Ces rendez-vous de vaccination contre la Covid-19 pourraient vous intéresser"
     message["From"] = config.email_address
     message["To"] = user['email_address']
 
-    msg = """<html><body><p>Voici des disponibilités de rendez-vous pour une première dose du vaccin contre la Covid-19 qui pourraient vous intéresser</p>"""
+    msg = """<html><body><h3>Voici des disponibilités de rendez-vous pour une première dose du vaccin contre la Covid-19 qui pourraient vous intéresser</h3>"""
 
     for place in establishments:
         if place['id'] in user['establishments_of_interest']:
-            msg = msg + """<p><b>""" + place['name_fr'] + "</b>: " + \
-                """ <i>""" + place['formatted_address'] + """</i></p>"""
-            establishment_availabilities = [a for a in availabilities if a['establishment'] == place['establishment']]
-            for availability in establishment_availabilities:
-                start_date, start_time = utils.get_datetime_strings(availability['start'])
-                msg = msg + """<p> - Le """ + start_date + " à " + start_time + """</p>"""
 
-    msg = msg + """</body></html>"""
+            previous_start_date = ''
+            establishment_availabilities = [a for a in availabilities if a['establishment'] == place['establishment']]
+
+            if len(establishment_availabilities) != 0:
+                msg = msg + """<p><h2>""" + place['name_fr'] + "</h2> " + \
+                            """ <i>""" + place['formatted_address'] + """</i></p>"""
+
+                for availability in establishment_availabilities:
+                    start_date, start_time = utils.get_datetime_full_strings(availability['start'])
+                    if start_date != previous_start_date:
+                        msg = msg + """<h4>""" + start_date + """<h4>"""
+                        previous_start_date = start_date
+                    msg = msg + start_time + ", "
+
+    msg = msg + """<p><b>Alerte Vaccin QC</b></p></body></html>"""
 
     html = MIMEText(msg, "html")
     message.attach(html)
@@ -85,8 +98,8 @@ def send_unsubscription_request(email_address, random_code):
     message["From"] = config.email_address
     message["To"] = email_address
 
-    msg = """<html><body><p>Voici votre code de confirmation pour vous désabonner du service Alerte Vaccin QC:</p>"""
-    msg = msg + """<h1>""" + str(random_code) + """</h1></body></html>"""
+    msg = """<html><body><h3>Voici votre code de confirmation pour vous désabonner du service Alerte Vaccin QC:</h3>"""
+    msg = msg + """<h1>""" + str(random_code) + """</h1><p><b>Alerte Vaccin QC</b></p></body></html>"""
 
     html = MIMEText(msg, "html")
     message.attach(html)
@@ -102,7 +115,9 @@ def send_unsubscription_confirmation(email_address):
     message["From"] = config.email_address
     message["To"] = email_address
 
-    msg = """<html><body><p>Vous venez de vous désabonner au service Alerte Vaccin QC</p></body></html>"""
+    msg = """<html><body><h2>Vous venez de vous désabonner du service Alerte Vaccin QC</h2>
+                <p>Merci d'avoir utilisé notre service</p>
+                <p><b>Alerte Vaccin QC</b></p></body></html>"""
 
     html = MIMEText(msg, "html")
     message.attach(html)
--- api/utils.py
+++ api/utils.py
@@ -5,6 +5,10 @@ from dateutil import tz
 from_zone = tz.gettz('UTC')
 to_zone = tz.gettz('America/New_York')
 
+# Months, there has to be a better way to do this
+months = {"01": "janvier", "02": "février", "03": "mars", "04": "avril", "05": "mai", "06": "juin",
+          "07": "juillet", "08": "aout", "09": "septembre", "10": "octobre", "11": "novembre", "12": "décembre"}
+
 
 def get_datetime(date_str, convert_from_utc=False):
     date_time = datetime.strptime(date_str[0:16], "%Y-%m-%dT%H:%M")
@@ -16,7 +20,13 @@ def get_datetime(date_str, convert_from_utc=False):
 
 def get_datetime_strings(date_str):
     date_time = get_datetime(date_str)
-    return date_time.strftime("%m/%d/%Y"), date_time.strftime("%H:%M:%S")
+    return date_time.strftime("%m/%d/%Y"), date_time.strftime("%H:%M")
+
+
+def get_datetime_full_strings(date_str):
+    date, time = get_datetime_strings(date_str)
+    date_full_string = date[3:5].replace("0", "") + " " + months.get(date[0:2]) + " " + date[6:12]
+    return date_full_string, time
 
 
 def identify_new_availabilities(previous_availabilities, current_availabilities):
@@ -33,7 +43,8 @@ def identify_availabilities_of_interest(current_availabilities, user):
 
     for availability in current_availabilities:
         start = get_datetime(availability['start'], True)
-        a = next((a for a in user['availabilities'] if get_datetime(a['start']) <= start <= get_datetime(a['stop'])), None)
+        a = next((a for a in user['availabilities'] if get_datetime(
+            a['start']) <= start <= get_datetime(a['stop'])), None)
 
         if a is not None and availability['place'] in user['establishments_of_interest']:
             availabilities_of_interest.append(availability)
