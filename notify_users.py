220525f4612127058a7412f107c79a224a0bdc8c
simkass <simkassab@gmail.com>
2021-05-02 21:30:33 -0400

    Changed notifications to be sent every hour

--- notify_users.py
+++ notify_users.py
@@ -24,5 +24,5 @@ def notify_users():
     db_client.update_availabilities(current_availabilities)
 
 
-schedule.add_job(notify_users, 'interval', minutes=15)
+schedule.add_job(notify_users, 'cron', hour='7-22')
 schedule.start()
